8ad882a851f7a2c79992dc037c90583392e7ffd8
christinae20492 <125934369+christinae20492@users.noreply.github.com>
2023-09-05 20:18:43 -0400

    html setup

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const btns=document.querySelectorAll(".btn")
 btns.forEach(function(btn){
 btn.addEventListener("click",function(e){
     const styles=e.currentTarget.classList;
-    if(styles.contains('decrease'){
+    if(styles.contains('decrease')){
         count--;
     }else if(styles.contains('increase')){
         count++;
     }else{count=0}
-    value.textContent=count;)
-})
-})
+    if(count>0){
+        value.style.color="green";
+    } if(count>0){value.style.color="red"}if(count===0){value.style.color="black"}
+    value.textContent=count;})})
